532b8de8206252bbba96bf38622ef4ed3f87acd9
mubswajhi <mubswajhi@gmail.com>
2022-02-04 14:42:02 +1100

    final commit

--- js/index.js
+++ js/index.js
@@ -30,19 +30,29 @@ window.addEventListener('DOMContentLoaded', () => {
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
   const quizArray = [
     {
-      q: 'Which is the third planet from the sun?',
+      q: '1. Which is the third planet from the sun?',
       o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
       a: 1, // array index 1 - so Earth is the correct answer here
     },
     {
-      q: 'Which is the largest ocean on Earth?',
+      q: '2. Which is the largest ocean on Earth?',
       o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
       a: 3,
     },
     {
-      q: 'What is the capital of Australia',
+      q: '3. What is the capital of Australia',
       o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
       a: 1,
+    },
+    {
+      q: ' 4.  Which country has the most volcanoes?',
+      o: ['Sydney', 'Indonesia','Bangladesh', 'Japan'],
+      a: 1,
+    },
+    {
+      q: '5. What percentage of the River Nile is located in Egypt?',
+      o: ['22%','44%', '34%', '75%'],
+      a: 0,
     },
   ];
 
@@ -52,7 +62,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let quizDisplay = '';
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
-                   Q - ${quizItem.q}
+                   Q ${quizItem.q}
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
@@ -62,10 +72,10 @@ window.addEventListener('DOMContentLoaded', () => {
       quizWrap.innerHTML = quizDisplay;
     });
   };
-
+  let score = 0;
   // Calculate the score
   const calculateScore = () => {
-    let score = 0;
+   
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
         //highlight the li if it is the correct answer
@@ -76,15 +86,64 @@ window.addEventListener('DOMContentLoaded', () => {
 
         if (quizItem.a == i) {
           //change background color of li element here
+          liElement.style.backgroundColor = "#e7b3ff"
         }
 
         if (radioElement.checked) {
           // code for task 1 goes here
+          if (quizItem.a == i) {
+            score =  score + 1;
+            }
         }
       }
     });
-  };
+    
+    let displayScore = document.getElementById('score');
+    displayScore.innerHTML = 'Score = ' + score +" / "+"5"
 
+  };
+  
   // call the displayQuiz function
   displayQuiz();
+  const btnSubmit = document.getElementById('btnSubmit');
+  btnSubmit.addEventListener("click",calculateScore);
+  // reset button
+  const btnReset = document.getElementById('btnReset');
+  btnReset.addEventListener("click",resetFunction);
+  function resetFunction () {
+   window.location.reload();
+  }
+
+  function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+            if (minutes == 0 && seconds == 0) {
+              //alert('Time Up')
+              calculateScore();
+              timer = 0;
+              score = 0;
+            }
+        }
+       
+    }, 1000);
+    
+    
+}
+
+window.onload = function () {
+    var oneMinutes = 60 * 1,
+        display = document.querySelector('#time');
+    startTimer(oneMinutes, display);
+};
+
 });
